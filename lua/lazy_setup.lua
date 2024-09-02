8d4fad0f69f9cd2d81e3c031544b6beb4c857b0f
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2024-09-02 23:19:52 +0200

    ts-error-translation addition

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,32 +1,42 @@
 require("lazy").setup({
-  {
-    "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
-    import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
-    },
-  },
-  { import = "community" },
-  { import = "plugins" },
-} --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax" } },
-  ui = { backdrop = 100 },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
+    {
+      "AstroNvim/AstroNvim",
+      version = "^4",                -- Remove version tracking to elect for nighly AstroNvim
+      import = "astronvim.plugins",
+      opts = {                       -- AstroNvim options must be set here with the `import` key
+        mapleader = " ",             -- This ensures the leader key must be configured before Lazy is set up
+        maplocalleader = ",",        -- This ensures the localleader key must be configured before Lazy is set up
+        icons_enabled = true,        -- Set to false to disable icons (if no Nerd Font is available)
+        pin_plugins = nil,           -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+        update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
       },
     },
-  },
-} --[[@as LazyConfig]])
+    {
+      "otavioschwanck/arrow.nvim",
+      opts = {
+        show_icons = true,
+        leader_key = ';',        -- Recommended to be a single key
+        buffer_leader_key = 'm', -- Per Buffer Mappings
+      }
+    },
+    { "dmmulroy/ts-error-translator.nvim", }, -- Matt Peacock ? implementations of typescript error tranlstor
+    { import = "community" },
+    { import = "plugins" },
+  } --[[@as LazySpec]],
+  {
+    -- Configure any other `lazy.nvim` configuration options here
+    install = { colorscheme = { "astrodark", "habamax" } },
+    ui = { backdrop = 100 },
+    performance = {
+      rtp = {
+        -- disable some rtp plugins, add more to your liking
+        disabled_plugins = {
+          "gzip",
+          "netrwPlugin",
+          "tarPlugin",
+          "tohtml",
+          "zipPlugin",
+        },
+      },
+    },
+  } --[[@as LazyConfig]])

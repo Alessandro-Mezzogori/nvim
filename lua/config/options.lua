4a10cf49c8726f3f0d86305a965e59316ca12c58
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2025-09-17 12:16:47 +0200

    mappings e test per uso con pdfviewer custom

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,3 +18,94 @@ if wsl then
     cache_enabled = 0,
   }
 end
+
+vim.g.lazyvim_prettier_needs_config = true
+
+-- TEMP WEBVIEW MAPPINGS
+
+function start_job(job)
+  return vim.fn.jobstart(job({
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        print("stdout, data: " .. vim.inspect(data))
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        print("stderr, data: " .. vim.inspect(data))
+      end
+    end,
+    on_exit = function(_, code)
+      print("Process exited with code: " .. vim.inspect(code))
+    end,
+  }))
+end
+
+-- TODO: support for multiple pdf inside same viewer
+CURRENT_PDF_JOB_ID = 0
+vim.api.nvim_create_user_command("Pdf", function(opts)
+  -- TODO: support for binary search / predefined folder
+  local bin = "E:/projects/personal/NeovimEdgePdfController/NeovimEdgePdfController/bin/Debug/net9.0-windows"
+  local exec = vim.fn.fnamemodify(bin .. "/" .. "NeovimEdgePdfController.exe", ":p")
+  exec =
+    "E:\\projects\\personal\\NeovimEdgePdfController\\NeovimEdgePdfController\\bin\\Debug\\net9.0-windows\\NeovimEdgePdfController.exe"
+
+  local arg = opts.args
+  local fullpath = arg
+  if not vim.fn.has("win32") == 1 then
+    -- Unix-like: absolute if starts with "/"
+    if not arg:match("^/") then
+      fullpath = vim.fn.fnamemodify(arg, ":p")
+    end
+  else
+    -- Windows: absolute if starts with drive letter (C:\) or UNC path (\\)
+    if not arg:match("^%a:[/\\]") and not arg:match("^\\\\") then
+      fullpath = vim.fn.fnamemodify(arg, ":p")
+    end
+  end
+
+  print("echo " .. exec)
+  print("echo " .. fullpath)
+
+  -- Run your executable asynchronously
+  -- TODO: cross platform ( ma devo fare anche l'eseguibile webview2)
+
+  if CURRENT_PDF_JOB_ID ~= 0 then
+    vim.fn.jobstop(CURRENT_PDF_JOB_ID)
+    CURRENT_PDF_JOB_ID = 0
+    CURRENT_PAGE = 0
+  end
+
+  CURRENT_PDF_JOB_ID = start_job({ "cmd.exe", "/c", "start", exec, fullpath })
+end, {
+  nargs = 1,
+  complete = "file",
+})
+
+CURRENT_PAGE = 0
+function set_page(page)
+  CURRENT_PAGE = page
+
+  local body = "page:" .. CURRENT_PAGE
+
+  -- TODO: page checks
+  -- TODO: retrieve info from active pdf from application
+  start_job({ "powershell", "Invoke-WebRequest", "-Uri", "http://localhost:12345", "-Method", "Post", "-Body", body })
+end
+
+vim.keymap.set("n", "<Leader>own", function()
+  set_page(CURRENT_PAGE + 1)
+end)
+
+vim.keymap.set("n", "<Leader>owp", function()
+  set_page(CURRENT_PAGE - 1)
+end)
+
+vim.api.nvim_create_user_command("PdfPage", function(opts)
+  set_page(opts.args)
+end, {
+  nargs = 1,
+  complete = "file",
+})
